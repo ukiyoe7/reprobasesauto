0ca7d7e22fcb2c1d54bcc8a0042a61979af89e01
Sandro Jk <sandro007@gmail.com>
2023-03-29 23:34:42 -0300

    OK

--- ADM_DASHBOARD.R
+++ ADM_DASHBOARD.R
@@ -11,7 +11,7 @@ library(reshape2)
 con2 <- dbConnect(odbc::odbc(), "reproreplica")
 
 ## SALES PERFORMANCE
-
+  
 df <- dbGetQuery(con2, statement = read_file("C:\\Users\\Repro\\Documents\\R\\ADM\\BASES_AUTO\\SQL\\RESULT_ADM.sql"))
 
 
@@ -25,26 +25,237 @@ range_write("17W3qQfIhG6yBAZFCCxVeumscNjGl-aM3FVz2tAr36wo", range = "A:D",data =
 
 df2 <- dbGetQuery(con2, statement = read_file("C:\\Users\\Repro\\Documents\\R\\ADM\\BASES_AUTO\\SQL\\PRODUTOS_ADM.sql")) %>% 
               mutate(PILARES=str_trim(PILARES)) %>% 
-               mutate(LENTES=str_trim(LENTES))
+               mutate(LENTES=str_trim(LENTES)) %>% 
+                mutate(TIPO=str_trim(TIPO)) %>% 
+                 mutate(MPROPRIA=str_trim(MPROPRIA))
 
 
 View(df2)
 
+## SHARE VARILUX ==============================================================
 
-df2 %>% group_by(floor_date(PEDDTBAIXA,"year")) %>% summarize(V=sum(VRVENDA))
+SHARE_VARILUX_MM <-
+  df2   %>%  filter(PILARES=='VARILUX') %>% 
+  group_by(MES=floor_date(PEDDTBAIXA,"month")) %>%  
+  summarize(SHARE = round(sum(VRVENDA),3)) %>% mutate(PILAR="VARILUX")
+
+
+SHARE_MULTIFOCAIS_MM <-
+  df2   %>%  filter(TIPO=='MULTIFOCAIS') %>% 
+  group_by(MES=floor_date(PEDDTBAIXA,"month")) %>%  
+  summarize(SHARE = round(sum(VRVENDA),3)) %>%  mutate(PILAR="MULTIFOCAIS")
+
+SHARE_VARILUX_MM_2 <-
+  rbind(SHARE_VARILUX_MM,SHARE_MULTIFOCAIS_MM) %>% 
+  group_by(MES) %>% 
+  summarize(SHARE=sum(SHARE[PILAR=='VARILUX'])/sum(SHARE[PILAR=='MULTIFOCAIS'])) 
+
+SHARE_VARILUX_YTD <-
+  df2   %>%  filter(PILARES=='VARILUX') %>% 
+   group_by(ANO=floor_date(PEDDTBAIXA,"year")) %>%  
+    summarize(SHARE = round(sum(VRVENDA),3)) %>% mutate(PILAR="VARILUX")
+
+
+SHARE_MULTIFOCAIS_YTD <-
+df2   %>%  filter(TIPO=='MULTIFOCAIS') %>% 
+  group_by(ANO=floor_date(PEDDTBAIXA,"year")) %>%  
+  summarize(SHARE = round(sum(VRVENDA),3)) %>%  mutate(PILAR="MULTIFOCAIS")
+
+
+rbind(SHARE_VARILUX_YTD,SHARE_MULTIFOCAIS_YTD) %>% 
+  group_by(ANO) %>% 
+   summarize(SHARE=sum(SHARE[PILAR=='VARILUX'])/sum(SHARE[PILAR=='MULTIFOCAIS'])) 
+
+
+
+
+
+
+## SHARE KODAK ==============================================================
+
+## MM 
+
+SHARE_KODAK_MM <-
+  df2   %>%  filter(PILARES=='KODAK') %>% 
+  group_by(MES=floor_date(PEDDTBAIXA,"month")) %>%  
+  summarize(SHARE = round(sum(VRVENDA),3)) %>% mutate(PILAR="KODAK")
+
+
+SHARE_LENTES_MM <-
+  df2   %>%  filter(LENTES=='LENTES') %>% 
+  group_by(MES=floor_date(PEDDTBAIXA,"month")) %>%  
+  summarize(SHARE = round(sum(VRVENDA),3)) %>%  mutate(PILAR="LENTES")
+
+SHARE_KODAK_MM_2 <-
+rbind(SHARE_KODAK_MM,SHARE_LENTES_MM) %>% 
+  group_by(MES) %>% 
+  summarize(SHARE=sum(SHARE[PILAR=='KODAK'])/sum(SHARE[PILAR=='LENTES'])) 
+
+## YTD
+
+SHARE_KODAK_YTD <-
+  df2   %>%  filter(PILARES=='KODAK') %>% 
+  group_by(ANO=floor_date(PEDDTBAIXA,"year")) %>%  
+  summarize(SHARE = round(sum(VRVENDA),3)) %>% mutate(PILAR="KODAK")
+
+
+SHARE_LENTES_YTD <-
+  df2   %>%  filter(LENTES=='LENTES') %>% 
+  group_by(ANO=floor_date(PEDDTBAIXA,"year")) %>%  
+  summarize(SHARE = round(sum(VRVENDA),3)) %>%  mutate(PILAR="LENTES")
+
+
+SHARE_KODAK_YTD_2 <-
+rbind(SHARE_KODAK_YTD,SHARE_LENTES_YTD) %>% 
+  group_by(ANO) %>% 
+  summarize(SHARE=sum(SHARE[PILAR=='KODAK'])/sum(SHARE[PILAR=='LENTES'])) 
+    
+
+## SHARE TRANSITIONS  ==============================================================
+
+## MM 
+
+SHARE_TRANSITIONS_MM <-
+  df2   %>%  filter(TRANSITIONS=='TRANSITIONS') %>% 
+  group_by(MES=floor_date(PEDDTBAIXA,"month")) %>%  
+  summarize(SHARE = round(sum(QTD),3)) %>% mutate(PILAR="TRANSITIONS")
+
+
+SHARE_LENTES_QTD_MM <-
+  df2   %>%  filter(LENTES=='LENTES') %>% 
+  group_by(MES=floor_date(PEDDTBAIXA,"month")) %>%  
+  summarize(SHARE = round(sum(QTD),3)) %>%  mutate(PILAR="LENTES")
+
+SHARE_TRANSITIONS_MM_2 <-
+rbind(SHARE_TRANSITIONS_MM,SHARE_LENTES_QTD_MM) %>% 
+  group_by(MES) %>% 
+  summarize(SHARE=sum(SHARE[PILAR=='TRANSITIONS'])/sum(SHARE[PILAR=='LENTES'])) 
+
+## YTD
+
+SHARE_TRANSITIONS_YTD <-
+  df2   %>%  filter(TRANSITIONS=='TRANSITIONS') %>% 
+  group_by(ANO=floor_date(PEDDTBAIXA,"year")) %>%  
+  summarize(SHARE = round(sum(QTD),3)) %>% mutate(PILAR="TRANSITIONS")
+
+
+SHARE_LENTES_QTD_YTD <-
+  df2   %>%  filter(LENTES=='LENTES') %>% 
+  group_by(ANO=floor_date(PEDDTBAIXA,"year")) %>%  
+  summarize(SHARE = round(sum(QTD),3)) %>%  mutate(PILAR="LENTES")
+
+SHARE_TRANSITIONS_YTD_2 <-
+rbind(SHARE_TRANSITIONS_YTD,SHARE_LENTES_QTD_YTD) %>% 
+  group_by(ANO) %>% 
+  summarize(SHARE=sum(SHARE[PILAR=='TRANSITIONS'])/sum(SHARE[PILAR=='LENTES'])) 
+
+
+## SHARE CRIZAL  ==============================================================
+
+## MM 
+
+SHARE_CRIZAL_MM <-
+  df2   %>%  filter(PILARES=='CRIZAL VS') %>% 
+  group_by(MES=floor_date(PEDDTBAIXA,"month")) %>%  
+  summarize(SHARE = round(sum(QTD),3)) %>% mutate(PILAR="CRIZAL")
+
+
+SHARE_LA_QTD_MM <-
+  df2   %>%  filter(TIPO=="LENTES ACABADAS") %>% 
+  group_by(MES=floor_date(PEDDTBAIXA,"month")) %>%  
+  summarize(SHARE = round(sum(QTD),3)) %>%  mutate(PILAR="LA")
+
+SHARE_CRIZAL_MM_2 <-
+rbind(SHARE_CRIZAL_MM,SHARE_LA_QTD_MM) %>% 
+  group_by(MES) %>% 
+  summarize(SHARE=sum(SHARE[PILAR=='CRIZAL'])/sum(SHARE[PILAR=='LA'])) 
+
+## YTD
+
+SHARE_CRIZAL_YTD <-
+  df2   %>%  filter(PILARES=='CRIZAL VS') %>% 
+  group_by(ANO=floor_date(PEDDTBAIXA,"year")) %>%  
+  summarize(SHARE = round(sum(QTD),3)) %>% mutate(PILAR="CRIZAL")
+
+
+SHARE_LA_QTD_YTD <-
+  df2   %>%  filter(TIPO=="LENTES ACABADAS") %>% 
+  group_by(ANO=floor_date(PEDDTBAIXA,"year")) %>%  
+  summarize(SHARE = round(sum(QTD),3)) %>%  mutate(PILAR="LA")
+
+SHARE_CRIZAL_YTD_2 <-
+rbind(SHARE_CRIZAL_YTD,SHARE_LA_QTD_YTD) %>% 
+  group_by(ANO) %>% 
+  summarize(SHARE=sum(SHARE[PILAR=='CRIZAL'])/sum(SHARE[PILAR=='LA'])) 
+
+
+## SHARE MARCAS REPRO  ==============================================================
+
+## MM 
+
+SHARE_MREPRO_MM <-
+  df2   %>%  filter(MPROPRIA=='M REPRO') %>% 
+  group_by(MES=floor_date(PEDDTBAIXA,"month")) %>%  
+  summarize(SHARE = round(sum(VRVENDA),3)) %>% mutate(PILAR="M REPRO")
+
+
+SHARE_VLENTES_MM <-
+  df2   %>%  filter(LENTES=='LENTES') %>% 
+  group_by(MES=floor_date(PEDDTBAIXA,"month")) %>%  
+  summarize(SHARE = round(sum(VRVENDA),3)) %>%  mutate(PILAR="V LENTES")
+
+SHARE_MREPRO_MM_2 <-
+rbind(SHARE_MREPRO_MM,SHARE_VLENTES_MM) %>% 
+  group_by(MES) %>% 
+  summarize(SHARE=sum(SHARE[PILAR=='M REPRO'])/sum(SHARE[PILAR=='V LENTES'])) 
+
+## YTD
+
+SHARE_MREPRO_YTD <-
+  df2   %>%  filter(MPROPRIA=='M REPRO') %>% 
+  group_by(ANO=floor_date(PEDDTBAIXA,"year")) %>%  
+  summarize(SHARE = round(sum(VRVENDA),3)) %>% mutate(PILAR='M REPRO')
+
+
+SHARE_VLENTES_YTD <-
+  df2   %>%  filter(LENTES=='LENTES') %>% 
+  group_by(ANO=floor_date(PEDDTBAIXA,"year")) %>%  
+  summarize(SHARE = round(sum(VRVENDA),3)) %>%  mutate(PILAR='V LENTES')
+
+SHARE_MREPRO_YTD_2 <- 
+rbind(SHARE_MREPRO_YTD,SHARE_VLENTES_YTD) %>% 
+  group_by(ANO) %>% 
+  summarize(SHARE=sum(SHARE[PILAR=='M REPRO'])/sum(SHARE[PILAR=='V LENTES'])) 
+
+
+
+
+## SHARE CRIZAL  ==============================================================
+
+
+SHARE_VARILUX <-
+  df2   %>%  filter(PILARES=='VARILUX') %>% 
+  group_by(ANO=floor_date(PEDDTBAIXA,"year"),PILARES) %>%  
+  summarize(
+    SHARE = round(sum(VRVENDA) / sum(.$VRVENDA[floor_date(.$PEDDTBAIXA, "year") == 
+                                                 floor_date(PEDDTBAIXA, "year") & 
+                                                 floor_date(.$PEDDTBAIXA, "year") == 
+                                                 floor_date(PEDDTBAIXA, "year")]), 3)) %>% 
+  melt(.,id.vars = c("ANO", "PILARES"),variable.name = "METRICA",value.name = "VALOR")
+
 
 ## SHARE PILARES
   
   
 SHARE_PILARES <-
   df2   %>%  filter(LENTES=='LENTES') %>% 
-  group_by(MES=floor_date(PEDDTBAIXA,"month"),PILARES) %>%  
+  group_by(ANO=floor_date(PEDDTBAIXA,"year"),PILARES) %>%  
   summarize(
             SHARE = round(sum(VRVENDA) / sum(.$VRVENDA[floor_date(.$PEDDTBAIXA, "year") == 
                                                                 floor_date(PEDDTBAIXA, "year") & 
-                                                                floor_date(.$PEDDTBAIXA, "month") == 
-                                                   floor_date(PEDDTBAIXA, "month")]), 3)) %>% 
-  melt(.,id.vars = c("MES", "PILARES"),variable.name = "METRICA",value.name = "VALOR")
+                                                                floor_date(.$PEDDTBAIXA, "year") == 
+                                                   floor_date(PEDDTBAIXA, "year")]), 3)) %>% 
+  melt(.,id.vars = c("ANO", "PILARES"),variable.name = "METRICA",value.name = "VALOR")
 
 
 SALES_PILARES <-
